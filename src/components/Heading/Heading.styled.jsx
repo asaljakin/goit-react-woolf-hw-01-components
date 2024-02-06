81bea65b6110ca7202ac6514c0aba7147ecf361b
Andrew <asaljakin@gmail.com>
2024-02-07 01:41:31 +0200

    bugfix exlarge

--- src/components/Heading/Heading.styled.jsx
+++ src/components/Heading/Heading.styled.jsx
@@ -1,7 +1,7 @@
 import styled from '@emotion/styled';
 
 export const Heading = styled.h2`
-  font-size: ${props => props.theme.fontSizes.large};
+  font-size: ${props => props.theme.fontSizes.exlarge};
   font-weight: 700;
   color: ${props => props.theme.colors.dark};
   text-shadow: 1px 1px 2px ${props => props.theme.colors.gray};
